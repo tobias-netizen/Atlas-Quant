76bc35859a3d27b92a73f86e958a6d095920c0de
Tobias Hyhlik <Tobias@hyhlik.com>
2026-07-26 23:30:17 +0200

    Added ATR and support and resistance and cleaned a few things up

--- main.py
+++ main.py
@@ -1,19 +1,71 @@
+#Download data and the list of tickers to analyze 
 from src.download_data import download_data
 tickers = ["SPY",
            "QQQ",
            "XLK"
 ]
+results = {}
 for ticker in tickers :
     data =download_data(ticker)
     print(data.tail())
     
-    
+# analysis of market in total and more specifically the highest lowest and open of price
     from src.analyze_market import market_summary
-    last_close, highest, lowest, returns = market_summary(data)
+    last_close, highest, lowest, returns,volume_average, today_volume, percentage_volume, volatility_level, atr = market_summary(data)
+    from src.analyze_market import calculate_trend 
+    score, ma20, ma200, distance_20, distance_200, trend = calculate_trend(data, last_close)
+    from src.analyze_market import calculate_52_week_levels
+    distance_from_high, distance_from_low = calculate_52_week_levels(last_close,highest, lowest)
+    from src.analyze_market import calculate_support_resistance
+    support_levels, resistance_levels = calculate_support_resistance(data, atr)
+
+    results[ticker] = {
+        "Current" : last_close,
+        "Highest" : highest,
+        "Lowest" : lowest,
+        #"Percentage change over 5 days" : returns,
+        "MA20" : ma20,
+        "MA200" : ma200,
+        "Score" : score, 
+        "Trend" : trend,
+        "Volume compared to average" : percentage_volume,
+        "Volatility" : volatility_level,
+        "Percentage distance from MA20" : distance_20,
+        "Percentage distance from MA200" : distance_200,
+        "Percentage from low" : distance_from_low,
+        "Percentage from high" : distance_from_high,
+        "Average true range" : atr
+    }
+    
     print(f"Latest: {last_close}")
     print(f"Highest: {highest}")
     print(f"Lowest: {lowest}")
     
+# Last five days of price action in terms of percentage
+    
+    
+    
     print(f"\nLast 5 daily returns:")
     for date, value in returns.tail().items():
        print(f"{date.date()} : {value * 100:.2f}%")
+# The calculation of trend for each ticker based on moving averages
+    
+    
+    
+    print(f"20 Day MA : {ma20:.2f}")
+    print(f"200 Day MA : {ma200:.2f}")
+    print(f"Distance from 20 MA: {distance_20:.2f}%")
+    print(f"Distance from 200 MA: {distance_200:.2f}%")
+    print (f"Trend Score : {score}/2")
+    print (f"Trend: {trend}")
+    print(f"Percentage comapred to average : {percentage_volume:.2f}%")
+    print(f"Volatility : {volatility_level}")
+    print(f"Percentage from high : {distance_from_high:.2f}%")
+    print(f"Percentage from low : {distance_from_low:.2f}%")
+
+    for level in resistance_levels:
+        print(f"Resistance: {level:.2f}")
+    for level in support_levels:
+        print(f"Support: {level:.2f}")
+
+    
--- src/analyze_market.py
+++ src/analyze_market.py
@@ -1,6 +1,113 @@
+import pandas as pd 
 def market_summary(data) : 
+    close = data["Close"].squeeze()
+    high = data["High"].squeeze()
+    low = data["Low"].squeeze()
     last_close = data["Close"].iloc[-1].item() 
     highest  = data["Close"].max().item()
     lowest = data["Close"].min().item()
     returns = data["Close"].squeeze().pct_change()
-    return last_close, highest, lowest, returns
+
+    # Change in Volume on the trading day
+    
+    volume = data["Volume"].squeeze()
+    volume_average  = volume.rolling(window=20).mean().iloc[-1].item()
+    today_volume = volume.iloc[-1].item()
+    percentage_volume = ((today_volume - volume_average) / volume_average)* 100
+
+    # Volatility 
+
+    volatility = returns.std().item()
+
+    if volatility < 0.01:
+        volatility_level = "low"
+    elif volatility < 0.02:
+        volatility_level = "Medium"
+    else:
+        volatility_level = "High"
+
+    # ATR
+
+    previous_close = close.shift(1)
+    range1 = high - low 
+    range2 = (high - previous_close).abs()
+    range3 = (low- previous_close).abs()
+    true_range = pd.concat([range1, range2, range3], axis=1).max(axis=1)
+    atr = true_range.rolling(window=14).mean().iloc[-1].item()
+
+    return last_close, highest, lowest, returns, volume_average, today_volume, percentage_volume, volatility_level, atr
+
+def calculate_trend(data, last_close):
+    ma20 = data["Close"].squeeze().rolling(window=20).mean().iloc[-1].item()
+    ma200 = data["Close"].squeeze().rolling(window=200).mean().iloc[-1].item()
+
+    score = 0 
+
+    if last_close > ma20: 
+        score += 1 
+    if ma20 > ma200:
+        score += 1
+    
+    if score == 2:
+        trend = "Bullish"
+    elif score == 1: 
+        trend = "Mixed"
+    else:
+        trend = "Bearish"
+
+    distance_20 = ((last_close - ma20) / ma20) * 100 
+    distance_200 = ((last_close - ma200) / ma200) * 100
+
+    return score, ma20, ma200, distance_20, distance_200, trend
+
+def calculate_52_week_levels(last_close,highest, lowest):
+    distance_from_high = ((last_close - highest)/ highest) * 100
+    distance_from_low = ((last_close - lowest)/ lowest) * 100
+    return distance_from_high, distance_from_low
+
+def calculate_support_resistance(data, atr): 
+    
+    last_close = data["Close"].iloc[-1].item()
+    high = data["High"].squeeze()
+    low = data["Low"].squeeze()
+    resistance_levels = []
+    support_levels = []
+    for i in range(1, len(high) - 1):
+        if high.iloc[i] > high.iloc[i-1] and high.iloc[1] > high.iloc[i +1]:
+            resistance_levels.append(high.iloc[1].item())
+        if low.iloc[i] < low.iloc[i-1] and low.iloc[1] < low.iloc[i +1]:
+            support_levels.append(low.iloc[i].item())
+    support_levels = sorted(set(support_levels))
+    resistance_levels = sorted(set(resistance_levels))
+    support_levels = [
+        level for level in support_levels
+        if level < last_close
+    ]
+    resistance_levels = [
+        level for level in resistance_levels 
+        if level > last_close
+    ]
+    support_levels.sort(reverse=True)
+    resistance_levels.sort()
+
+    merged_support  = []
+    merged_resistance = []
+
+    for level in support_levels:
+        if not merged_support:
+            merged_support.append(level)
+        elif abs(level - merged_support[-1]) > atr * 0.25:
+            merged_support.append(level)
+    support_levels = merged_support
+    
+    
+    for level in resistance_levels:
+        if not merged_resistance :
+            merged_resistance.append(level)
+        elif abs(level - merged_resistance[-1]) > atr * 0.25:
+            merged_resistance.append(level)
+    resistance_levels = merged_resistance
+
+    support_levels = support_levels[:3]
+    resistance_levels = resistance_levels[:3]
+    return support_levels, resistance_levels
